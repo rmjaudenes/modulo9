787cf5f419b2ad7a5d3563b054d912413b1e0f61
rmjaudenes <rmjaudenes@gmail.com>
2015-06-15 16:26:50 +0200

    Paso 3: Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
+
 var express = require('express');
 var router = express.Router();
 
+//importar el enrutador
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
- // res.render('index', { title: 'Express' });
   res.render('index', { title: 'Quiz' });
 });
 
+//el enrutador es esta pagina, index.js, ha de importar el controlador
+//llevan a ejecutar las acciones referiadas a question y answer
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer',   quizController.answer);
+
+
 module.exports = router;
+
