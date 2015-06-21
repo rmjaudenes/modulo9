31003386773ad9eec3fcc275b488de1f4dd2401a
rmjaudenes <rmjaudenes@gmail.com>
2015-06-21 03:34:00 +0200

    Paso 8: Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -3,7 +3,27 @@
 //construimos nuestro modelo
 
 var path = require('path');
+
+//configurar la bd con las variables database_url y database_storage
+//Postgress DATABASE_URL = postgres://user:passwd@host:post/database
+//SQlite    DATABASE_URL = sqlite://:@:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+
+
+// CARGAR MODELO ORM
+
 var Sequelize = require('sequelize');
+
 //luego se crea la variable sequelize
 //luego se crea un objeto con este modulo
 //clase squelize, como si fuera la clase de bases de datos
@@ -11,8 +31,14 @@ var Sequelize = require('sequelize');
 //el dialecto que es squlite yluego storage, fichero donde se guardaran
 //los datos
 
-var sequelize = new Sequelize(null, null, null, 
-                       {dialect: "sqlite", storage: "quiz.sqlite"}
+var sequelize = new Sequelize(DB_name, user, pwd, 
+                       {dialect: protocol,
+                       protocol: protocol,
+                       port: port,
+                       host: host,
+                       storage: storage,
+                       omitNull: true
+                       }
                     );
 
 
